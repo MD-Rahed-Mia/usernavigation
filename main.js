14ae7fc873f9c3a77b149dee938a1a9e3e278e2c
mdrahed2023 <135752931+mdrahed2023@users.noreply.github.com>
2023-10-16 22:07:15 +0600

    message

---
+++ main.js
@@ -0,0 +1,7 @@
+
+let menuToggle = document.querySelector(".menu_toggle");
+let navigation = document.querySelector(".navigation");
+
+menuToggle.addEventListener("click", function () {
+  navigation.classList.toggle("active");
+})
